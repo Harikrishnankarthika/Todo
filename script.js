30981d6b30816f168180fc0ae26ae65bf6ae45bd
Hari <harikrishnan53828@gmail.com>
2022-03-25 15:35:58 +0530

    firstcoomit

---
+++ script.js
@@ -0,0 +1,10 @@
+const inputbox = document.querySelector(".inputField input" );
+const add8tn = document.querySelector(".inputField button");
+
+
+
+inputbox.onkeyup = () => 
+{
+    let userDate = inputbox.ariaValueMax;
+    if(userDate.trim() !)
+}
